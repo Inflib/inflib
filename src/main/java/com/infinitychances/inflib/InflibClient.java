30debd3632ba3928dbbab85b4434b11bd665c9dc
RC908 <rubixcube908@gmail.com>
2025-02-04 19:38:21 -0500

    client readded- possible final

---
+++ src/main/java/com/infinitychances/inflib/InflibClient.java
@@ -0,0 +1,10 @@
+package com.infinitychances.inflib;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InflibClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
