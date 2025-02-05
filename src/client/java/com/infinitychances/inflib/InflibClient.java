45b4a0613d34ade1be741f21db5469d63ec046e1
RC908 <rubixcube908@gmail.com>
2025-02-04 19:36:37 -0500

    attempt to rework system

---
+++ src/client/java/com/infinitychances/inflib/InflibClient.java
@@ -0,0 +1,10 @@
+package com.infinitychances.inflib;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InflibClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/infinitychances/inflib/InflibDataGenerator.java
@@ -0,0 +1,11 @@
+package com.infinitychances.inflib;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InflibDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
