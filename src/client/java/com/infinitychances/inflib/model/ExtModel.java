ce87b23f9d1303a0e92a3d39e0e48dabf00dc0a2
InfinityChances <rubixcube908@gmail.com>
2025-07-14 11:08:04 -0400

    Changed how the ExtModel class handles its creation and lowered LOC in constructor

--- src/client/java/com/infinitychances/inflib/model/ExtModel.java
+++ src/client/java/com/infinitychances/inflib/model/ExtModel.java
@@ -34,51 +34,37 @@ public class ExtModel{
         this.requiredKeys = null;
     }
 
-    //constructor for an ExtModel without a need for a variant
-    private ExtModel(String ModOrigin, ExtModelType type, String name, TextureKey... textures) {
+    //Base constructor that gets referenced using of
+    private ExtModel(String modOrigin, ExtModelType type, String name, Optional<String> variant, TextureKey... textures) {
         if(name.contains("/")) {
-            LOGGER.error("Invalid Parent Model");
-            throw new IllegalArgumentException("Parent Model Cannot contain a /!");
+            LOGGER.error("Invalid Parent Model: ");
+            throw new IllegalArgumentException("Parent Model cannot contain a /!");
         }
         switch (type) {
             case BLOCK_VARIANT:
-                LOGGER.error("NEEDS VARIANT");
+                if(variant.isEmpty()) {
+                    throw new IllegalArgumentException("Cannot use Block Variant without providing a valid variant!");
+                }
+                this.model = block(modOrigin, name, variant, textures);
+                this.path = "block/" + name + variant.orElse("");
                 break;
             case BLOCK:
-                this.model = block(ModOrigin, name, textures);
+                this.model = block(modOrigin, name, textures);
                 this.path = "block/" + name;
                 break;
             case ITEM:
-                this.model = item(ModOrigin, name, textures);
+                this.model = item(modOrigin, name, textures);
                 this.path = "item/" + name;
                 break;
             case NORMAL:
                 this.model = make(textures);
-                this.path = "unavailable/"+ name;
+                this.path = name;
                 break;
             default:
                 LOGGER.error("Invalid Type");
         }
-        String tempId = getID(type, name);
-        if (!checkId(tempId)){
-            throw new InvalidInputException("DUPLICATE MODEL BEING CREATED", type + " " + name);
-        }
-        this.id = tempId;
-        idMap.put(this.id, this);
-
-        this.requiredKeys = textures;
-        this.modOrigin = ModOrigin;
-        this.type = type;
-    }
-
-    //constructor if there is a variant
-    private ExtModel(String ModOrigin, ExtModelType type, String name, String variant, TextureKey... textures) {
-        if(name.contains("/")) {
-            LOGGER.error("Invalid Parent Model");
-            throw new IllegalArgumentException("Parent Model Cannot contain a /!");
-        }
         if (type == ExtModelType.BLOCK_VARIANT) {
-            this.model = block(ModOrigin, name, variant, textures);
+            this.model = block(modOrigin, name, variant, textures);
             this.path = "block/" + name;
         } else {
             LOGGER.error("Variant Not Supported With this type");
@@ -91,40 +77,40 @@ public class ExtModel{
         idMap.put(this.id, this);
 
         this.requiredKeys = textures;
-        this.modOrigin = ModOrigin;
+        this.modOrigin = modOrigin;
         this.type = type;
     }
 
-    public static ExtModel of(String ModOrigin, ExtModelType type, String name, String variant, TextureKey... textures) {
-        return new ExtModel(ModOrigin,type, name,variant,textures);
+    public static ExtModel of(String modOrigin, ExtModelType type, String name, String variant, TextureKey... textures) {
+        return new ExtModel(modOrigin,type, name, Optional.of(variant),textures);
     }
 
-    public static ExtModel of(String ModOrigin, ExtModelType type, String name, TextureKey... textures) {
-        return new ExtModel(ModOrigin,type, name,textures);
+    public static ExtModel of(String modOrigin, ExtModelType type, String name, TextureKey... textures) {
+        return new ExtModel(modOrigin,type, name,Optional.empty(),textures);
     }
 
     public static ExtModel of(ExtModelType type, String name, String variant, TextureKey... textures) {
-        return new ExtModel("minecraft",type, name,variant,textures);
+        return new ExtModel("minecraft",type, name,Optional.of(variant),textures);
     }
 
     public static ExtModel of(ExtModelType type, String name, TextureKey... textures) {
-        return new ExtModel("minecraft",type, name,textures);
+        return new ExtModel("minecraft",type,name,Optional.empty(),textures);
     }
 
     private static Model make(TextureKey... requiredTextureKeys) {
         return new Model(Optional.empty(), Optional.empty(), requiredTextureKeys);
     }
 
-    private static Model block(String ModOrigin, String name, TextureKey... requiredTextureKeys) {
-        return new Model(Optional.of(Identifier.of(ModOrigin, "block/" + name)), Optional.empty(), requiredTextureKeys);
+    private static Model block(String modOrigin, String name, TextureKey... requiredTextureKeys) {
+        return new Model(Optional.of(Identifier.of(modOrigin, "block/" + name)), Optional.empty(), requiredTextureKeys);
     }
 
-    private static Model item(String ModOrigin, String name, TextureKey... requiredTextureKeys) {
-        return new Model(Optional.of(Identifier.of(ModOrigin, "item/" + name)), Optional.empty(), requiredTextureKeys);
+    private static Model item(String modOrigin, String name, TextureKey... requiredTextureKeys) {
+        return new Model(Optional.of(Identifier.of(modOrigin, "item/" + name)), Optional.empty(), requiredTextureKeys);
     }
 
-    private static Model block(String ModOrigin, String name, String variant, TextureKey... requiredTextureKeys) {
-        return new Model(Optional.of(Identifier.of(ModOrigin, "block/" + name)), Optional.of(variant), requiredTextureKeys);
+    private static Model block(String modOrigin, String name, Optional<String> variant, TextureKey... requiredTextureKeys) {
+        return new Model(Optional.of(Identifier.of(modOrigin, "block/" + name)), variant, requiredTextureKeys);
     }
 
     //returns the ExtModel of the id
