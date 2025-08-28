3e4cd741bd9dcfef2066b07717afb9841c5aeab0
InfinityChances <rubixcube908@gmail.com>
2025-08-27 21:15:06 -0400

    javadoc

--- src/main/java/com/infinitychances/inflib/exceptions/MissingValueException.java
+++ src/main/java/com/infinitychances/inflib/exceptions/MissingValueException.java
@@ -1,8 +1,9 @@
 package com.infinitychances.inflib.exceptions;
 
-
-//thrown when a value in a class is missing
-//ie. when mod id does not get defined when using the offer methods in recipe datagen
+/**
+ * Used when an instance variable was not defined, usually through a set method.
+ * @author InfinityChances
+ */
 public class MissingValueException extends RuntimeException {
     public MissingValueException(String message) {
         super(message);
