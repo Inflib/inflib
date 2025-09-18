b89803d89d3a6f68609190c26cb634f688f4c5a6
InfinityChances <rubixcube908@gmail.com>
2025-09-17 20:50:32 -0400

    forgot to remove extra fluff

--- src/main/java/com/infinitychances/inflib/InfLib.java
+++ src/main/java/com/infinitychances/inflib/InfLib.java
@@ -29,9 +29,5 @@ public class InfLib implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-        EnterBoatCallback.EVENT.register((player, hand, boat) -> {
-            player.giveItemStack(new ItemStack(Items.DIAMOND, 16));
-            return ActionResult.PASS;
-        });
 	}
 }
