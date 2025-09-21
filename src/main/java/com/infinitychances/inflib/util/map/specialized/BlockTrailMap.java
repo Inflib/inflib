eb5599160cd81ebc9701acc38e5e022996998209
InfinityChances <rubixcube908@gmail.com>
2025-09-21 13:51:12 -0400

    beginning of new strategy to add specialized trailmaps

---
+++ src/main/java/com/infinitychances/inflib/util/map/specialized/BlockTrailMap.java
@@ -0,0 +1,15 @@
+package com.infinitychances.inflib.util.map.specialized;
+
+import com.infinitychances.inflib.util.map.SingleTrailMap;
+import com.jcraft.jorbis.Block;
+
+public class BlockTrailMap extends SingleTrailMap<Block> {
+    public BlockTrailMap() {
+        super();
+    }
+
+    public BlockTrailMap(String key) {
+        super(key);
+
+    }
+}
