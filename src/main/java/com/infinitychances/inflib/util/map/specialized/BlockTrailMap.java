91787eed61cc9e00cc209902a23ca704a8820e7e
InfinityChances <rubixcube908@gmail.com>
2025-10-02 14:15:03 -0400

    added new features to specialized trailmaps

--- src/main/java/com/infinitychances/inflib/util/map/specialized/BlockTrailMap.java
+++ src/main/java/com/infinitychances/inflib/util/map/specialized/BlockTrailMap.java
@@ -1,7 +1,11 @@
 package com.infinitychances.inflib.util.map.specialized;
 
 import com.infinitychances.inflib.util.map.SingleTrailMap;
-import com.jcraft.jorbis.Block;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntry;
 
 public class BlockTrailMap extends SingleTrailMap<Block> {
     public BlockTrailMap() {
@@ -10,6 +14,30 @@ public class BlockTrailMap extends SingleTrailMap<Block> {
 
     public BlockTrailMap(String key) {
         super(key);
+    }
+
+    public Float getBlastResistancefromId(Integer id) {
+        return getFromId(id).getBlastResistance();
+    }
+
+    public Float getSlipperinessfromId(Integer id) {
+        return getFromId(id).getSlipperiness();
+    }
+
+    public Float getVelocityMultiplierfromId(Integer id) {
+        return getFromId(id).getVelocityMultiplier();
+    }
+
+    public BlockState getDefaultStatefromId(Integer id) {
+        return getFromId(id).getDefaultState();
+    }
+
+    public Item getBlockItemfromId(Integer id) {
+        return getFromId(id).asItem();
+    }
+
+    public RegistryEntry<Block> getBlockRegistryEntryfromId(Integer id) {
+        return Registries.BLOCK.getEntry(getFromId(id));
     }
 
     /*
